571ac20cc4e7eda0ad417572a3505c39983836b4
Limkon <130622161+Limkon@users.noreply.github.com>
2024-02-17 17:23:32 +0800

    Update send_telegram_message.py

--- send_telegram_message.py
+++ send_telegram_message.py
@@ -1,13 +1,14 @@
+import os  # 添加 os 模块的导入语句
 import random
 import requests
 
-# Generate a random message.
+# 生成随机消息
 with open('message.txt', 'r') as f:
     messages = f.read().splitlines()
 message = random.choice(messages)
 
-# Send the message to your friend.
-url = f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}/sendMessage'
+# 将消息发送给你的朋友
+url = f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}/sendMessage'  # 使用 os.environ 访问环境变量
 data = {'chat_id': os.environ["USER_ID"], 'text': message}
 response = requests.post(url, data=data)
 print(response.json())
